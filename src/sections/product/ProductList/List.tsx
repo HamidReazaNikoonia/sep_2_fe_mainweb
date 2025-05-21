2aed3d54066934086a24e9db7797dccd0ff9e33b
hamidrezanikoonia <hamid.nikoonia@ecashpay.com.ph>
2025-05-22 03:23:56 +0330

    minor changes

--- src/sections/product/ProductList/List.tsx
+++ src/sections/product/ProductList/List.tsx
@@ -5,11 +5,29 @@ interface ListProps {
   products: IProduct[]
 }
 
+const sampleProduct =  {
+  id: "number",
+  title: 'محصول',
+  subtitle: "توضیحات محصول در این قسمت قرار میگیرد",
+  meta_title: "",
+  meta_description: "",
+  slug: "string",
+  description: "توضیحات",
+  brand: "string",
+  average_rating: 3,
+  countInStock: 20,
+  is_available: true,
+  status: true,
+  qr_code: "string",
+  price: 50000,
+
+}
+
 export default function List({ products }: ListProps) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 px-4 md:px-0">
       {products?.length !== 0 && products.map(product => (
-        <ProductCard product={product} key={product._id} />
+        <ProductCard product={sampleProduct} key={product._id} />
       ))}
 
       {products?.length === 0 && (
